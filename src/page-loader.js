354877b09e278187455dc2a249cc37cc6c565a1e
Ivan Protsko <ivanprotsko@ivanprotsko.com>
2023-08-09 20:21:13 +0800

    add some refactoring

--- src/page-loader.js
+++ src/page-loader.js
@@ -4,7 +4,15 @@ import jsdom from 'jsdom';
 import _ from 'lodash';
 import path from 'path';
 
-export const getPath = (directory, url, format) => `${directory}/${createName(url, format)}`;
+const config = {
+  tags: [
+    { tag: 'link', attribute: 'href', },
+    { tag: 'img', attribute: 'src', },
+    { tag: 'a', attribute: 'href', },
+    { tag: 'script', attribute: 'src', },
+  ],
+  fileFormats: ['png', 'jpg', 'jpeg', 'gif', 'svg', 'css', 'js'],
+}
 
 export const createFolder = async (path) => {
   try {
@@ -34,16 +42,19 @@ export const createName = (url, format) => {
   return [name, format].join(separator);
 };
 
-const requiredTags = [
-  { tag: 'link', attribute: 'href', },
-  { tag: 'img', attribute: 'src', },
-  { tag: 'a', attribute: 'href', },
-  { tag: 'script', attribute: 'src', },
-];
-
+const changeDomWithLocalHrefPaths = (elements, directory) => {
+    elements.map((elementObject) => {
+        const { element, assetsFolderPath, href, fileFormat } = elementObject;
+        const filePath = `${directory}/${createName(href, fileFormat)}`;
+        const absolutePath = path.resolve(filePath);
+        const attribute = element.hasAttribute('src') ? 'src' : 'href';
+        element[attribute] = absolutePath;
+    });
+    return elements;
+}
 const getHtmlElementLinkAttribute = (element) => element.hasAttribute('src') ? 'src' : 'href';
 
-export const selectAssetElements = (dom, tags) => {
+export const selectAssetElements = (dom, tags, fileFormats) => {
   const document = dom.window.document;
 
   const elements = tags.map(({ tag, attribute }) => {
@@ -53,15 +64,15 @@ export const selectAssetElements = (dom, tags) => {
     .filter((element) => {
       const attribute = getHtmlElementLinkAttribute(element);
       const fileFormat = getFileFormat(element[attribute]);
-      const searchingFormats = [fileFormats.base64, fileFormats.utf8].flat();
-      return searchingFormats.includes(fileFormat);
+      return fileFormats.includes(fileFormat);
+    })
+    .map((element) => {
+        const href = getHtmlElementHref(element);
+        const fileFormat = getFileFormat(href);
+        const fileName = createName(href, fileFormat);
+        return { element, href, fileName, fileFormat };
     });
   return elements;
-};
-
-export const getDataTypeByHref = (format) => {
-  if (format === 'png' || format === 'jpg' || format === 'jpeg' || format === 'gif') return 'base64';
-  else return 'utf8';
 };
 
 export const getHtmlElementHref = (element) => {
@@ -69,70 +80,25 @@ export const getHtmlElementHref = (element) => {
   return element[attribute];
 };
 
-const fileFormats = {
-  base64: ['png', 'jpg', 'jpeg', 'gif'],
-  utf8: ['svg', 'css', 'js'],
-};
-
-const getDataType = (format, formats) => {
-  let type = '';
-  formats.base64.forEach((fileFormat) => {
-    if (fileFormat === format) type = 'base64';
-  });
-  formats.utf8.forEach((fileFormat) => {
-    if (fileFormat === format) type = 'utf8';
-  });
-  return type;
-};
+const getBinaryDataFromUrl = async (href) => {
+    return await axios.get(href, { responseType: 'arraybuffer' })
+        .then((response) => Buffer.from(response.data, 'binary').toString('binary'));
+}
+const writeBinaryData = async (data, path) => await fsp.writeFile(path, data, 'binary');
 
 const getFileFormat = (path) => path.split('.').pop();
 export const downloadAssetElements = (elements, filesFolder) => {
-  const paths = [];
-
-  elements.map(async (element) => {
-    const href = getHtmlElementHref(element);
-    const fileFormat = getFileFormat(href);
-    const dataType = getDataType(fileFormat, fileFormats);
-    const filePath = getPath(filesFolder, href, fileFormat, 'file');
-
-    paths.push(filePath);
-
-    switch (dataType) {
-      case 'base64': await axios.get(href, { responseType: 'arraybuffer' })
-          .then((response) => Buffer.from(response.data, 'binary').toString('base64'))
-          .then((data) => fsp.writeFile(filePath, data, dataType));
-        break;
-      case 'utf8': await axios.get(href)
-          .then((response) => fsp.writeFile(filePath, response.data, dataType));
-        break;
-      default: console.log('\'dataType\' undefined');
-    }
+  return elements.map(async (element) => {
+    const data = await getBinaryDataFromUrl(element.href);
+    await writeBinaryData(data, `${filesFolder}/${element.fileName}`);
   });
-
-  return paths;
 };
 
-const updateAssetHtmlElementUrls = (elementsList) => {
-  elementsList.map(([ element, path ]) => {
-    const attribute = element.hasAttribute('src') ? 'src' : 'href';
-    element[attribute] = path;
-  });
-}
-
-const joinAssetElementsAndTheirPaths = (elements, paths) => {
-  const list = [];
-  for (let i = 0; i < elements.length; i += 1) {
-    const absolutePath = path.resolve(paths[i]);
-    list.push([elements[i], absolutePath]);
-  }
-  return list;
-}
-
 export default async (url, directory) => {
-  const assetsFolderPath =  getPath(directory, url, 'files')
-  let assetElements;
-  let assetElementPaths;
+  const assetsFolderPath = `${directory}/${createName(url, 'files')}`;
+  const { tags, fileFormats } = config;
   let pageDom;
+  let finalHtml;
 
   console.log(path.resolve('tmp'));
   await doesFolderExist(directory)
@@ -141,12 +107,15 @@ export default async (url, directory) => {
     .catch(() => createFolder(assetsFolderPath))
     .then(() => axios.get(url))
     .then((response) => pageDom = new jsdom.JSDOM(response.data))
-    .then((dom) => assetElements = selectAssetElements(dom, requiredTags))
-    .then((assets) => assetElementPaths = downloadAssetElements(assets, assetsFolderPath))
-    .then(() => joinAssetElementsAndTheirPaths(assetElements, assetElementPaths ))
-    .then((elementsList) => updateAssetHtmlElementUrls(elementsList))
-    .then(() => fsp.writeFile(
-      getPath(directory, url, 'html', 'file'),
-      pageDom.window.document.documentElement.innerHTML,
-      {encoding: 'utf8'}));
+    .then((dom) => selectAssetElements(dom, tags, fileFormats))
+    .then((elementObjects) => finalHtml = changeDomWithLocalHrefPaths(elementObjects, assetsFolderPath))
+    .then((elementObjects) => downloadAssetElements(elementObjects, assetsFolderPath))
+    .then(() => {
+      const finalHtml = pageDom.window.document.documentElement.innerHTML;
+      fsp.writeFile(
+        `${directory}/${createName(url, 'html')}`,
+        finalHtml,
+        {encoding: 'utf8'}
+      )
+    });
 };
