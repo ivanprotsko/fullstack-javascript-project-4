f578f2cf4ad7e250cac7c0329ecff3ca5fcfb2ce
Ivan Protsko <ivanprotsko@ivanprotsko.com>
2023-08-14 21:54:56 +0800

    add test fixes

--- __tests__/get-data-from-url.test.js
+++ __tests__/get-data-from-url.test.js
@@ -5,12 +5,8 @@ test('Get data from URL', async () => {
   const scope = nock('https://ru.hexlet.io')
     .get('/courses')
     .reply(200, 'data');
-  try {
-    await expect(
-      getBinaryDataFromUrl('https://ru.hexlet.io/courses'),
-    ).resolves.toBe('data');
-  } catch (e) {
-    console.error(e);
-  }
+  await expect(
+    getBinaryDataFromUrl('https://ru.hexlet.io/courses'),
+  ).resolves.toBe('data');
   scope.done();
 });
