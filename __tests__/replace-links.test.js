fc181ee6f0ba4474cc9454445a7a2c8ca9e1b174
Ivan Protsko <ivanprotsko@ivanprotsko.com>
2023-08-24 13:49:40 +0800

    add getFinalUrl function fix

--- __tests__/replace-links.test.js
+++ __tests__/replace-links.test.js
@@ -3,20 +3,18 @@ import { describe, test, expect } from '@jest/globals';
 import replaceLinks from '../src/utils/replace-links.js';
 import parseTags from '../src/utils/parse-string/parse-tags.js';
 import extractTagParams from '../src/utils/generate-structure/extract-tag-params.js';
+import extractLinksFromHtml from "../src/utils/generate-structure/extract-links-from-html.js";
 
 describe('Test of pageLoaderApp function', () => {
   test('Success case', async () => {
     const url = 'https://ru.hexlet.io/courses';
-
+    const searchedTags = ['script', 'a', 'link', 'img'];
     const initial = await fsp.readFile(`${process.cwd()}/__fixtures__/initial.html`, 'utf8');
     const final = await fsp.readFile(`${process.cwd()}/__fixtures__/final.html`, 'utf8');
-
     const tags = parseTags(initial, ['script', 'a', 'link', 'img']);
-    const links = tags
-      .map((tag) => extractTagParams(tag, url))
-      .filter((link) => link.urlFinal !== null);
-    console.log(links);
-    const processedHtml = replaceLinks(initial, links);
+    const assetFileLinks = extractLinksFromHtml(initial, searchedTags, url, 'host');
+
+    const processedHtml = replaceLinks(initial, assetFileLinks);
     expect(processedHtml).toBe(final);
   });
 });
--- src/utils/get-path/get-final-url-path.js
+++ src/utils/get-path/get-final-url-path.js
@@ -7,7 +7,7 @@ export default (o) => {
     urlInitial, targetUrl, urlHost, urlType, targetHost, urlParams: { dir, name, ext },
   } = o;
 
-  if (urlHost !== 'localhost' && urlHost !== targetHost) return null;
+  if (urlHost !== 'localhost' && urlHost !== targetHost) return urlInitial;
   if (urlType === 'page-html') return urlInitial;
   if (urlType === 'canonical-page') return getAssetFilePath(targetUrl, dir, name, '.html', targetHost);
   if (urlType === 'asset-file') return getAssetFilePath(targetUrl, dir, name, ext, targetHost);
