dfa32f1672110f5dfb7b80ff9b79cb25d6efaf0f
Ivan Protsko <ivanprotsko@ivanprotsko.com>
2023-08-24 13:51:54 +0800

    add eslint fixes

--- __tests__/replace-links.test.js
+++ __tests__/replace-links.test.js
@@ -1,9 +1,7 @@
 import fsp from 'fs/promises';
 import { describe, test, expect } from '@jest/globals';
 import replaceLinks from '../src/utils/replace-links.js';
-import parseTags from '../src/utils/parse-string/parse-tags.js';
-import extractTagParams from '../src/utils/generate-structure/extract-tag-params.js';
-import extractLinksFromHtml from "../src/utils/generate-structure/extract-links-from-html.js";
+import extractLinksFromHtml from '../src/utils/generate-structure/extract-links-from-html.js';
 
 describe('Test of pageLoaderApp function', () => {
   test('Success case', async () => {
@@ -11,7 +9,6 @@ describe('Test of pageLoaderApp function', () => {
     const searchedTags = ['script', 'a', 'link', 'img'];
     const initial = await fsp.readFile(`${process.cwd()}/__fixtures__/initial.html`, 'utf8');
     const final = await fsp.readFile(`${process.cwd()}/__fixtures__/final.html`, 'utf8');
-    const tags = parseTags(initial, ['script', 'a', 'link', 'img']);
     const assetFileLinks = extractLinksFromHtml(initial, searchedTags, url, 'host');
 
     const processedHtml = replaceLinks(initial, assetFileLinks);
